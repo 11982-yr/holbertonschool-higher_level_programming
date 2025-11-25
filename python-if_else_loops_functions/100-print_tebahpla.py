26abfe7da231fa18085be041afc0167536c1b9cc
Yara Alrasheed <11982@holbertonstudents.com>
2025-11-25 12:12:39 +0300

    Refactor alternating case alphabet script to use one print function and meet project constraints

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 
 for index, i in enumerate(range(122, 96, -1)):
-    if index % 2 == 0:
-        print(chr(i), end="")
-    else:
-        print(chr(i - 32), end="")
+    print("{}".format(chr(i if index % 2 == 0 else i - 32)), end="")
