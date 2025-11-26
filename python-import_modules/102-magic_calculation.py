2bbfe5b9a74c13848a8d731700f22e8039b6d017
Yara Alrasheed <11982@holbertonstudents.com>
2025-11-26 13:53:55 +0300

    Implement magic_calculation based on bytecode

--- python-import_modules/102-magic_calculation.py
+++ python-import_modules/102-magic_calculation.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 from magic_calculation_102 import add, sub
 
 def magic_calculation(a, b):
@@ -7,6 +6,6 @@ def magic_calculation(a, b):
         c = add(a, b)
         for i in range(4, 6):
             c = add(c, i)
-            return c
+        return c
     else:
         return sub(a, b)
