76c6789c96742bff3eb2971ed23898c4c2a02898
root <11982@holbertonstudents.com>
2025-11-30 16:33:46 +0000

    Implement add_tuple with safe element extraction

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 
+
 def add_tuple(tuple_a=(), tuple_b=()):
     if len(tuple_a) >= 1:
         a1 = tuple_a[0]
     else:
         a1 = 0
-    
+
     if len(tuple_a) >= 2:
         a2 = tuple_a[1]
     else:
@@ -20,5 +21,5 @@ def add_tuple(tuple_a=(), tuple_b=()):
         b2 = tuple_b[1]
     else:
         b2 = 0
-    
+
     return (a1 + b1, a2 + b2)
