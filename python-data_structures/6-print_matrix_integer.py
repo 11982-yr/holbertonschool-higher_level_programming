1b716e506b3d75f45d646c314d68abb539cdb4c0
Yara Alrasheed <11982@holbertonstudents.com>
2025-11-30 19:24:31 +0300

    Implement print_matrix_integer to print integer matrix

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,6 +3,9 @@
 
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
+        if len(row) == 0:
+            print()
+        else:
         for i, num in enumerate(row):
             if i < len(row) - 1:
                 print("{:d}".format(num), end=" ")
