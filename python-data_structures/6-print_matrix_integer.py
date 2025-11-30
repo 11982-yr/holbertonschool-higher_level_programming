e6c07dd238a47d2bce1d634a61cd4ebf18d1a17d
root <11982@holbertonstudents.com>
2025-11-30 16:25:48 +0000

    Implement print_matrix_integer to print integer matrix

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -10,4 +10,4 @@ def print_matrix_integer(matrix=[[]]):
                 if i < len(row) - 1:
                     print("{:d}".format(num), end=" ")
             else:
-                    print("{:d}".format(num))
+                print("{:d}".format(num))
