086b3123bfef851bc1a6a1a85e902ae866c154df
Yara Alrasheed <11982@holbertonstudents.com>
2025-12-01 15:11:37 +0300

    Implement uniq_add function to sum unique integers from a list.

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,1 +1,13 @@
+#!/usr/bin/python3
 
+
+def uniq_add(my_list=[]):
+    seen = []
+    total = 0
+  
+    for num in my_list:
+        if num not in seen:
+            total += num
+            seen.append(num)
+    return total
+        
