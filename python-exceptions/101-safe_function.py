ffe590ea812191fbcd317a8f6d4bdec4dec3f60f
Yara Alrasheed <11982@holbertonstudents.com>
2025-12-05 21:22:10 +0300

    Update 101-safe_function.py

--- python-exceptions/101-safe_function.py
+++ python-exceptions/101-safe_function.py
@@ -1,1 +1,19 @@
+#!/usr/bin/python3
+import sys
 
+
+def safe_function(fct, *args):
+    """Execute a function safely.
+
+    Args:
+        fct: function to execute.
+        *args: arguments to pass to the function.
+
+    Returns:
+        The result of fct(*args), or None if an exception occurs.
+    """
+    try:
+        return fct(*args)
+    except Exception as e:
+        print("Exception: {}".format(e), file=sys.stderr)
+        return None
