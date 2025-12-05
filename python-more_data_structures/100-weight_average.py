83287865faed458391f2e570a3d9b3fa4ab5f296
Yara Alrasheed <11982@holbertonstudents.com>
2025-12-05 21:17:10 +0300

    Update 100-weight_average.py

--- python-more_data_structures/100-weight_average.py
+++ python-more_data_structures/100-weight_average.py
@@ -1,1 +1,18 @@
+#!/usr/bin/python3
+"""Returns the weighted average of a list of tuples."""
+
+
+def weight_average(my_list=[]):
+
+    if not my_list:
+        return 0
+
+    total = 0
+    weight_sum = 0
+
+    for score, weight in my_list:
+        total += score * weight
+        weight_sum += weight
+
+    return total / weight_sum
 
