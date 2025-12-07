c965a58015fe4307fc2640cd79cf3a5f18ad6a69
Yara Alrasheed <11982@holbertonstudents.com>
2025-12-07 17:22:51 +0300

    A class Square that defines a square by:
    rivate instance attribute: size
    Instantiation with optional size: def __init__(self, size=0):
    - size must be an integer, otherwise raise a TypeError exception with the message size must be an integer
    - if size is less than 0, raise a ValueError exception with the message size must be >= 0

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,1 +1,16 @@
+#!/usr/bin/python3
+"""
+This module defines a square by:
+Private instance attribute: size
+Instantiation with optional size
+"""
 
+
+class Square:
+  """Represents a square"""
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
