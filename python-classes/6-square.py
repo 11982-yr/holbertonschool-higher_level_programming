fda7ce73ccf869d70e808cf75c0edd871d8eca42
Yara Alrasheed <11982@holbertonstudents.com>
2025-12-07 18:30:32 +0300

    A class Square that defines a square by:
    Private instance attribute: size
    Private instance attribute: position
    Instantiation with optional size and optional position
    Public instance method that returns the current square area
    Public instance method that prints in stdout the square with the character #

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -1,1 +1,60 @@
+#!/usr/bin/python3
+"""
+This module defines a square by:
+Private instance attribute: size
+Private instance attribute: position
+Instantiation with optional size and optional position
+Public instance method that returns the current square area
+Public instance method that prints in stdout the square with the character #
+"""
 
+
+class Square:
+    """Represents a Square"""
+    def __init__(self, size=0, position=(0, 0)):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+        self.position = position
+
+    def area(self):
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def my_print(self):
+        if self.__size == 0:
+            print("")
+            return
+        for _ in range(self.__position[1]):
+            print("")
+        for _ in range(self.__size):
+            print(" " * self.__position[0] + "#" * self.__size)
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if not isinstance(value, tuple):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if not isinstance(value[0], int) or not isinstance(value[1], int):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if value[0] < 0 or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
