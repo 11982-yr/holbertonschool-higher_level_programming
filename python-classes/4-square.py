dcc361a94916e150c15e488ef6b32a1b2812745c
Yara Alrasheed <11982@holbertonstudents.com>
2025-12-07 17:49:09 +0300

    Pycodestyle validation fix

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -17,11 +17,14 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
     def area(self):
         return self.__size * self.__size
+
     @property
     def size(self):
         return self.__size
+
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
