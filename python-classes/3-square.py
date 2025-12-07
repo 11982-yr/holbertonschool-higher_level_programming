f654e8ecdff6a0498a0bc24e4347ac210b8d5111
Yara Alrasheed <11982@holbertonstudents.com>
2025-12-07 17:34:39 +0300

    A class Square that defines a square by:
    Private instance attribute: size
    Instantiation with optional size: def __init__(self, size=0)
    Public instance method: def area(self): that returns the current square area

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,1 +1,20 @@
+#!/usr/bin/python3
+"""
+This module defines
+Private instance attribute: size
+Instantiation with optional size
+Public instance method: def area(self): 
+that returns the current square area
+"""
 
+
+class Square:
+    """Represents a square"""
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    def area(self):
+        return self.__size * self.__size
