9421d6bd4f10759e663d97d8613d214e204b0ef3
Yara Alrasheed <11982@holbertonstudents.com>
2025-12-07 17:48:22 +0300

    Add getter and setter to square

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,1 +1,31 @@
+#!/usr/bin/python3
+"""
+This module defines a square by:
+Private instance attribute: size
+Instantiation with optional size
+Public instance method: def area(self):
+that returns the current square area
+A getter and setter
+"""
 
+
+class Square:
+    """Represents a Square"""
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    def area(self):
+        return self.__size * self.__size
+    @property
+    def size(self):
+        return self.__size
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
