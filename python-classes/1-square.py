95484112964350dbd1a574877c4363d3b05830f2
Yara Alrasheed <11982@holbertonstudents.com>
2025-12-07 17:08:33 +0300

    A class Square that defines a square by:
    Private instance attribute: size
    Instantiation with size (no type/value verification)

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,1 +1,11 @@
+#!/usr/bin/python3
+"""
+This module defines a square by:
+Private instance attribute: size
+Instantiation with size
+"""
 
+
+class Square:
+    def __init__(self, size):
+        self.__size = size
