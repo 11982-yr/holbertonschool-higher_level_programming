1e3fc465dc571e4aa8c72d56c72746f59d06ff2c
Yara Alrasheed <11982@holbertonstudents.com>
2025-12-09 17:51:27 +0300

    Complete Task 5: Detect instance deletion
    - Added __del__ method to print "Bye rectangle..." when a Rectangle instance is deleted
    - Ensured __repr__ returns a recreatable string for eval()
    - Maintained proper width/height validation and '#' string output
    - Fully aligned with project requirements for 5-rectangle.py

--- python-more_classes/5-rectangle.py
+++ python-more_classes/5-rectangle.py
@@ -1,1 +1,60 @@
+#!/usr/bin/python3
+"""
+This module defines a Rectangle by:
+Private instance attributes: width and height
+Instantiation with optional width and height
+Public instance methods: area and perimeter
+String representation using '#'
+repr() that can recreate a new instance with eval()
+Prints a message when an instance is deleted
+"""
 
+
+class Rectangle:
+    """Represents a Rectangle"""
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.__width * self.__height
+
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        return "\n".join(["#" * self.__width for _ in range(self.__height)])
+
+    def __repr__(self):
+        return "Rectangle({}, {})".format(self.__width, self.__height)
+
+    def __del__(self):
+        print("Bye rectangle...")
