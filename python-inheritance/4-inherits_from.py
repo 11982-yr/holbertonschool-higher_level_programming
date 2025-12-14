421cc875f66b8e96e21d1ae88c6350c34c956153
Yara Alrasheed <11982@holbertonstudents.com>
2025-12-14 19:57:44 +0300

    Add inherits_from function

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,1 +1,10 @@
+#!/usr/bin/python3
+"""This module returns True if the object is an instance
+of a class that inherited (directly or indirectly)
+from the specified class; otherwise False
+"""
 
+
+def inherits_from(obj, a_class):
+    """Return True if obj is an instance of a class that inherited from a_class."""
+    return isinstance(obj, a_class) and type(obj) is not a_class
