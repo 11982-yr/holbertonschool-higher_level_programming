113abbda1d18670573a284100331b43e1edcf511
Yara Alrasheed <11982@holbertonstudents.com>
2025-12-14 19:59:02 +0300

    Update 4-inherits_from.py

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """This module returns True if the object is an instance
 of a class that inherited (directly or indirectly)
-from the specified class; otherwise False
+from another specified class; otherwise False.
 """
 
 
