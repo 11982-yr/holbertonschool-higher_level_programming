37d77a7dbef6c08e210ec9b1f2a85ba94ab9bfd9
Yara Alrasheed <11982@holbertonstudents.com>
2025-12-14 20:01:47 +0300

    Implement inherits_from helper

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
-"""This module returns True if the object is an instance
-of a class that inherited (directly or indirectly)
-from another specified class; otherwise False.
-"""
+"""Determine whether an object comes from a subclass of a given class."""
 
 
 def inherits_from(obj, a_class):
-    """Return True if obj is an instance of a class that inherited from a_class."""
-    return isinstance(obj, a_class) and type(obj) is not a_class
+    """Return True when obj is an instance of a subclass of a_class."""
+    return issubclass(type(obj), a_class) and type(obj) != a_class
