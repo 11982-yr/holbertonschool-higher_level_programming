a73254e5b221efaf556135382addbaac5e8549d5
Yara Alrasheed <11982@holbertonstudents.com>
2025-12-22 17:36:47 +0300

    Fix formatting and add newline at end of file

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-"""Module that provides a function to return the JSON representation
-   of an object."""
+"""Module that provides a function to return the JSON representation of an object."""
+
+import json
 
 
 def to_json_string(my_obj):
