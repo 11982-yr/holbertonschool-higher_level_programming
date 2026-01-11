0d79054f98a8a8fa92c3ab410ed6bf82e0b1a7c4
Yara Alrasheed <11982@holbertonstudents.com>
2026-01-11 11:40:23 +0300

    5. Full description

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,5 +1,4 @@
 -- 5. Full description
--- Prints a description of the table first_table from the database hbtn_0c_0
+-- Prints the full description of the table first_table from the database hbtn_0c_0
 
-USE hbtn_0c_0;
 SHOW CREATE TABLE first_table;
