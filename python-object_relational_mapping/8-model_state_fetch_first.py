9e68fdaf22fce9bb2dbea33730770a24fc5f1851
Yara Alrasheed <11982@holbertonstudents.com>
2026-01-13 13:16:30 +0300

    8. First state

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -22,7 +22,7 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    states = session.query(State).order_by(State.id).all()
+    state = session.query(State).order_by(State.id).first()
 
     if state is None:
         print("Nothing")
